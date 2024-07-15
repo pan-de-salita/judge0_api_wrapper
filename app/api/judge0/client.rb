a5bfdb27747ad8cfbd2c3b544e3ab8a7bd7e19fa
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-16 00:01:51 +0800

    feat: add calls for writing and reading a submission

--- app/api/judge0/client.rb
+++ app/api/judge0/client.rb
@@ -1,57 +1,31 @@
 # frozen_string_literal: true
 
-# The Judge0::Client class serves as a high-level interface for interacting with the Judge0 API.
-# It abstracts the underlying HTTP request mechanism, providing a straightforward way to access
-# various endpoints related to problems, languages, and other aspects of the Judge0 platform.
 module Judge0
-  # == Overview
-  #
-  # This class offers methods corresponding to common operations you'd perform with the Judge0 API,
-  # such as fetching problem statuses, retrieving information about programming languages, and
-  # accessing individual language details.
-  #
-  # == Methods
-  #
-  # - #status
-  #   Retrieves the current status of problems submitted to the Judge0 system.
-  #
-  # - #languages
-  #   Fetches a list of currently active programming languages supported by Judge0.
-  #
-  # - #all_languages
-  #   Returns a list of both active and archived programming languages.
-  #
-  # - #language(id:)
-  #   Obtains detailed information about a specific programming language by its ID.
-  #
-  # == Examples
-  #
-  # Fetch the current status of problems:
-  #   Judge0::Client.statuses
-  #
-  # Retrieve a list of active programming languages:
-  #   Judge0::Client.languages
-  #
-  # Get information about all programming languages:
-  #   Judge0::Client.all_languages
-  #
-  # Get details about a specific programming language by ID:
-  #   Judge0::Client.language(id: 72) # -> Returns information on Ruby
   class Client
     def self.statuses
-      Request.call(http_method: 'get', endpoint: '/statuses')
+      Request.call(http_method: :get, endpoint: '/statuses')
     end
 
     def self.languages
-      Request.call(http_method: 'get', endpoint: '/languages')
+      Request.call(http_method: :get, endpoint: '/languages')
     end
 
     def self.all_languages
-      Request.call(http_method: 'get', endpoint: '/languages/all')
+      Request.call(http_method: :get, endpoint: '/languages/all')
     end
 
-    def self.language(id:)
-      Request.call(http_method: 'get', endpoint: "/languages/#{id}")
+    def self.language(language_id:)
+      Request.call(http_method: :get, endpoint: "/languages/#{language_id}")
+    end
+
+    def self.write_submission(source_code:, language_id:, stdin: nil)
+      Request.call(http_method: :post, endpoint: '/submissions/?base64_encoded=false&wait=false',
+                   body: { 'source_code': source_code, 'language_id': language_id, 'stdin': stdin })
+    end
+
+    def self.read_submission(token:)
+      Request.call(http_method: :get,
+                   endpoint: "/submissions/#{token}?base64_encoded=false&fields=stdout,stderr,status_id,language_id")
     end
   end
 end
--- app/api/judge0/request.rb
+++ app/api/judge0/request.rb
@@ -1,24 +1,5 @@
 # frozen_string_literal: true
 
-# The Judge0::Request class provides a simplified interface for making HTTP requests to the Judge0 API.
-#
-# It encapsulates the setup for the Faraday HTTP client, including the base URL and authentication token,
-# allowing users to easily send requests to various endpoints of the Judge0 API.
-#
-# == Examples
-#
-#   # Send a GET request to fetch problem statuses
-#   Judge0::Request.call(http_method: :get, endpoint: '/statuses')
-#
-# == Available Endpoints
-#
-# This class supports all endpoints available in the Judge0 API. For a full list of supported endpoints,
-# refer to the official Judge0 API documentation.
-#
-# == Note
-#
-# The `call` method dynamically determines the HTTP method based on the provided symbol. Ensure that the
-# symbol matches one of the HTTP methods supported by the Judge0 API (e.g., :get, :post).
 module Judge0
   BASE_URL = 'https://judge0-ce.p.rapidapi.com'
   AUTHENTICATION_TOKEN = ENV['X_RAPIDAPI_KEY']
@@ -27,14 +8,11 @@ module Judge0
     headers: { 'x-rapidapi-key': AUTHENTICATION_TOKEN, 'x-rapidapi-host': 'judge0-ce.p.rapidapi.com' }
   )
 
-  # Sends an HTTP request to the specified endpoint of the Judge0 API.
-  #
-  # @param [String] http_method: The HTTP method to use for the request (:get, :post, etc.)
-  # @param [String] endpoint: The endpoint path relative to the base URL
-  # @return [Hash]: The parsed response body as a hash
   class Request
-    def self.call(http_method:, endpoint:)
-      CONNECTION.send(http_method.to_sym, endpoint)
+    def self.call(http_method:, endpoint:, body: nil)
+      result = body.nil? ? CONNECTION.send(http_method, endpoint) : CONNECTION.send(http_method, endpoint, body)
+
+      { status: result.status, reason_phrase: result.reason_phrase, data: JSON.parse(result.body) }
     end
   end
 end
