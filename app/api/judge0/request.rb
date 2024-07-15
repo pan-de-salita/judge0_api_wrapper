0793c950c67376562f8db471bdab4d56a9585a5c
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-16 01:29:54 +0800

    chore: reformat Request methods

--- app/api/judge0/request.rb
+++ app/api/judge0/request.rb
@@ -3,9 +3,10 @@
 module Judge0
   BASE_URL = 'https://judge0-ce.p.rapidapi.com'
   AUTHENTICATION_TOKEN = ENV['X_RAPIDAPI_KEY']
-  CONNECTION = Faraday.new(url: BASE_URL,
-                           headers: { 'x-rapidapi-key': AUTHENTICATION_TOKEN,
-                                      'x-rapidapi-host': 'judge0-ce.p.rapidapi.com' }) do |faraday|
+  CONNECTION = Faraday.new(
+    url: BASE_URL,
+    headers: { 'x-rapidapi-key': AUTHENTICATION_TOKEN, 'x-rapidapi-host': 'judge0-ce.p.rapidapi.com' }
+  ) do |faraday|
     faraday.response :raise_error
   end
 
@@ -13,10 +14,18 @@ module Judge0
     def self.call(http_method:, endpoint:, body: nil)
       result = body.nil? ? CONNECTION.send(http_method, endpoint) : CONNECTION.send(http_method, endpoint, body)
 
-      { status: result.status, reason_phrase: result.reason_phrase,
-        submissions_ramaining: result.headers['x-ratelimit-submissions-remaining'], data: JSON.parse(result.body) }
+      {
+        status: result.status,
+        reason_phrase: result.reason_phrase,
+        submissions_ramaining: result.headers['x-ratelimit-submissions-remaining'],
+        data: JSON.parse(result.body)
+      }
     rescue Faraday::Error => e
-      { status: e.response_status, message: Errors.translate(e.response_status), data: e.response_body }
+      {
+        status: e.response_status,
+        message: Errors.translate(e.response_status),
+        data: e.response_body
+      }
     end
   end
 end
