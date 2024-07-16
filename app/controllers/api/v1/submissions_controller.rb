d6a85830a7e57c98e7bc04473dce03a1450a260a
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-17 00:09:25 +0800

    refactor: rename create_submission_params to write_submission_params

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -6,14 +6,14 @@ module Api
       # attr_accessor :submission_token
 
       def create
-        create_submission = Judge0::Client.write_submission(
-          source_code: create_submission_params[:source_code],
-          language_id: create_submission_params[:language_id],
-          stdin: create_submission_params[:stdin]
+        submission = Judge0::Client.write_submission(
+          source_code: write_submission_params[:source_code],
+          language_id: write_submission_params[:language_id],
+          stdin: write_submission_params[:stdin]
         )
 
         # @submission_token = create_submission[:data]['token'] if create_submission[:status].between(200, 299)
-        render json: create_submission
+        render json: submission
       end
 
       def show
@@ -23,7 +23,7 @@ module Api
 
       private
 
-      def create_submission_params
+      def write_submission_params
         params.permit %i[source_code language_id stdin]
       end
 
