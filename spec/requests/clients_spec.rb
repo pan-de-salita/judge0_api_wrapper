817630bc5dada96423dc9e3274661c3da01a0d46
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-07-18 17:39:02 +0800

    feat: add submission tests

--- spec/requests/clients_spec.rb
+++ spec/requests/clients_spec.rb
@@ -3,7 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Judge0::Client, type: :request do
+  # Base URL for the Judge0 API
   let(:base_url) { 'https://judge0-ce.p.rapidapi.com' }
+
+  # Headers including the API key and host
   let(:headers) do
     {
       'Content-Type': 'application/json',
@@ -11,6 +14,8 @@ RSpec.describe Judge0::Client, type: :request do
       'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
     }
   end
+
+  # Default response body structure
   let(:base_body) do
     {
       'status' => 200,
@@ -20,7 +25,16 @@ RSpec.describe Judge0::Client, type: :request do
     }
   end
 
+  # Body for writing a new submission
+  let(:write_submission_body) do
+    {
+      'source_code' => "def add(a, b)\n  p a + b\nend\n\nadd(5, 5)",
+      'language_id' => 72
+    }
+  end
+
   describe '.statuses' do
+    # Data representing the possible statuses returned by the API
     let(:statuses_data) do
       { 'data' => [
         { 'id' => 1, 'description' => 'In Queue' },
@@ -28,16 +42,21 @@ RSpec.describe Judge0::Client, type: :request do
         { 'id' => 3, 'description' => 'Accepted' }
       ] }
     end
+
+    # Merged base body with statuses data, converted to JSON
     let(:statuses_body) { base_body.merge(statuses_data).to_json }
 
     it 'makes a GET request to /statuses' do
+      # Stub the request to simulate a successful response
       stub_request(:get, "#{base_url}/statuses")
         .with(headers:)
         .to_return(status: 200, body: statuses_body)
 
+      # Call the statuses method and parse the response
       response = Judge0::Client.statuses
       parsed_response_data = JSON.parse(response[:data])
 
+      # Assert that the response has the expected properties
       expect(response[:status]).to eq(200)
       expect(parsed_response_data['reason_phrase']).to eq('OK')
       expect(parsed_response_data['submissions_remaining']).to eq(50)
@@ -48,6 +67,7 @@ RSpec.describe Judge0::Client, type: :request do
   end
 
   describe '.languages' do
+    # Data representing the list of programming languages supported by the API
     let(:languages_data) do
       { 'data' => [
         { 'id' => 45, 'name' => 'Assembly (NASM 2.14.02)' },
@@ -55,16 +75,21 @@ RSpec.describe Judge0::Client, type: :request do
         { 'id' => 47, 'name' => 'Basic (FBC 1.07.1)' }
       ] }
     end
+
+    # Merged base body with languages data, converted to JSON
     let(:languages_body) { base_body.merge(languages_data).to_json }
 
     it 'makes a GET request to /languages' do
+      # Stub the request to simulate a successful response
       stub_request(:get, "#{base_url}/languages")
         .with(headers:)
         .to_return(status: 200, body: languages_body)
 
+      # Call the languages method and parse the response
       response = Judge0::Client.languages
       parsed_response_data = JSON.parse(response[:data])
 
+      # Assert that the response has the expected properties
       expect(response[:status]).to eq(200)
       expect(parsed_response_data['reason_phrase']).to eq('OK')
       expect(parsed_response_data['submissions_remaining']).to eq(50)
@@ -75,6 +100,7 @@ RSpec.describe Judge0::Client, type: :request do
   end
 
   describe '.all_languages' do
+    # Data representing the complete list of programming languages, including archived ones
     let(:all_languages_data) do
       { 'data' => [
         { 'id' => 45, 'name' => 'Assembly (NASM 2.14.02)', 'is_archived' => false },
@@ -82,16 +108,21 @@ RSpec.describe Judge0::Client, type: :request do
         { 'id' => 1, 'name' => 'Bash (4.4)', 'is_archived' => true }
       ] }
     end
+
+    # Merged base body with all languages data, converted to JSON
     let(:all_languages_body) { base_body.merge(all_languages_data).to_json }
 
     it 'makes a GET request to /languages/all' do
+      # Stub the request to simulate a successful response
       stub_request(:get, "#{base_url}/languages/all")
         .with(headers:)
         .to_return(status: 200, body: all_languages_body)
 
+      # Call the all_languages method and parse the response
       response = Judge0::Client.all_languages
       parsed_response_data = JSON.parse(response[:data])
 
+      # Assert that the response has the expected properties
       expect(response[:status]).to eq(200)
       expect(parsed_response_data['reason_phrase']).to eq('OK')
       expect(parsed_response_data['submissions_remaining']).to eq(50)
@@ -102,6 +133,7 @@ RSpec.describe Judge0::Client, type: :request do
   end
 
   describe '.language' do
+    # Data representing a single programming language
     let(:language_data) do
       { 'data' => {
         'id' => 72,
@@ -112,16 +144,21 @@ RSpec.describe Judge0::Client, type: :request do
         'run_cmd' => '/usr/local/ruby-2.7.0/bin/ruby script.rb'
       } }
     end
+
+    # Merged base body with language data, converted to JSON
     let(:language_body) { base_body.merge(language_data).to_json }
 
     it 'makes a GET request to /language/language_id' do
+      # Stub the request to simulate a successful response
       stub_request(:get, "#{base_url}/languages/#{language_data['data']['id']}")
         .with(headers:)
         .to_return(status: 200, body: language_body)
 
+      # Call the language method and parse the response
       response = Judge0::Client.language(language_id: language_data['data']['id'])
       parsed_response_data = JSON.parse(response[:data])
 
+      # Assert that the response has the expected properties
       expect(response[:status]).to eq(200)
       expect(parsed_response_data['reason_phrase']).to eq('OK')
       expect(parsed_response_data['submissions_remaining']).to eq(50)
@@ -131,13 +168,43 @@ RSpec.describe Judge0::Client, type: :request do
     end
   end
 
-  ##############################################################################
-  # Need actual API calls for the following?
-  ##############################################################################
-
   describe '.write_submission' do
+    it 'makes a POST request to /submissions/?base64_encoded=false&wait=false' do
+      # Use VCR cassette to record and replay the HTTP interaction
+      VCR.use_cassette('judge0/client_write_submission') do
+        # Make the submission and capture the response
+        response = Judge0::Client.write_submission(
+          source_code: write_submission_body['source_code'],
+          language_id: write_submission_body['language_id']
+        )
+
+        # Assert that the submission was successful and check the response details
+        expect(response[:status]).to be(201)
+        expect(response[:reason_phrase]).to eq('Created')
+        expect(response[:submissions_remaining] < 50).to be_truthy
+        expect(response[:data]['token']).to_not be_blank
+      end
+    end
   end
 
   describe '.read_submission' do
+    it 'makes a POST request to /submissions/submission_token?base64_encoded=false&fields=...' do
+      # Use VCR cassette to record and replay the HTTP interaction
+      VCR.use_cassette('judge0/client_read_submission') do
+        # Retrieve the submission token from the previously recorded cassette
+        write_submission_data = YAML.load_file('spec/cassettes/judge0/client_write_submission.yml')
+        submission_token = JSON.parse(write_submission_data['http_interactions'].first['response']['body']['string'])['token']
+
+        # Read the submission using the token and assert the response details
+        response = Judge0::Client.read_submission(token: submission_token)
+
+        expect(response[:status]).to be(200)
+        expect(response[:reason_phrase]).to eq('OK')
+        expect(response[:submissions_remaining] < 50).to be_truthy
+        expect(response[:data]['source_code']).to eq(write_submission_body['source_code'])
+        expect(response[:data]['language_id']).to eq(write_submission_body['language_id'])
+        expect(response[:data]['stdout'].chomp.to_i).to eq(10)
+      end
+    end
   end
 end
